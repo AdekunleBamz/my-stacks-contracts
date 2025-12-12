56bca3d7d088374ded864e57f39699b336f6456f
oyingrace <oyingrace307@gmail.com>
2025-12-12 14:18:27 +0100

    Refactor App component to integrate wallet connection functionality using @stacks/connect, enabling users to connect and disconnect their wallets while displaying their BNS name or address.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,42 @@
+import { connect, disconnect } from '@stacks/connect'
+import type { GetAddressesResult } from '@stacks/connect/dist/types/methods'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  let [isConnected, setIsConnected] = useState<boolean>(false)
+  let [walletInfo, setWalletInfo] = useState<any>(null)
+  let [bns, setBns] = useState<string>('')
 
+  async function connectWallet() {
+    let connectionResponse: GetAddressesResult = await connect()
+    let bnsName = await getBns(connectionResponse.addresses[2].address)
+
+    setIsConnected(true)
+    setWalletInfo(connectionResponse)
+    setBns(bnsName)
+  }
+
+  async function disconnectWallet() {
+    disconnect();
+  }
+  
+  async function getBns(stxAddress: string) {
+    let response = await fetch(`https://api.bnsv2.com/testnet/names/address/${stxAddress}/valid`)
+    let data = await response.json()
+
+    return data.names[0].full_name
+  }
+  
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h3>Stacks Dev Quickstart Message Board</h3>
+      {isConnected ? (
+        <button onClick={disconnectWallet}>{
+          bns ? bns : walletInfo.addresses[2].address
+        }</button>
+      ) : (
+        <button onClick={connectWallet}>connect wallet</button>
+      )}
     </>
   )
 }
-
-export default App
